d5aa58959494859697c927740fb5f9574d0cb7d0
devj <power3xz@gmail.com>
2022-07-16 16:52:15 +0900

    린트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-import { useEditor, EditorContent, Editor } from "@tiptap/react";
-import StarterKit from "@tiptap/starter-kit";
-import { useEffect, useState } from "react";
-import "./App.css";
+import React from 'react';
+import { useEditor, EditorContent, Editor } from '@tiptap/react';
+import StarterKit from '@tiptap/starter-kit';
+import { useEffect, useState } from 'react';
+import './App.css';
 
 const MenuBar = ({ editor }: { editor: Editor | null }) => {
   if (!editor) {
@@ -11,25 +12,25 @@ const MenuBar = ({ editor }: { editor: Editor | null }) => {
     <>
       <button
         onClick={() => editor.chain().focus().toggleBold().run()}
-        className={editor.isActive("bold") ? "is-active" : ""}
+        className={editor.isActive('bold') ? 'is-active' : ''}
       >
         bold
       </button>
       <button
         onClick={() => editor.chain().focus().toggleItalic().run()}
-        className={editor.isActive("italic") ? "is-active" : ""}
+        className={editor.isActive('italic') ? 'is-active' : ''}
       >
         italic
       </button>
       <button
         onClick={() => editor.chain().focus().toggleStrike().run()}
-        className={editor.isActive("strike") ? "is-active" : ""}
+        className={editor.isActive('strike') ? 'is-active' : ''}
       >
         strike
       </button>
       <button
         onClick={() => editor.chain().focus().toggleCode().run()}
-        className={editor.isActive("code") ? "is-active" : ""}
+        className={editor.isActive('code') ? 'is-active' : ''}
       >
         code
       </button>
@@ -41,67 +42,67 @@ const MenuBar = ({ editor }: { editor: Editor | null }) => {
       </button>
       <button
         onClick={() => editor.chain().focus().setParagraph().run()}
-        className={editor.isActive("paragraph") ? "is-active" : ""}
+        className={editor.isActive('paragraph') ? 'is-active' : ''}
       >
         paragraph
       </button>
       <button
         onClick={() => editor.chain().focus().toggleHeading({ level: 1 }).run()}
-        className={editor.isActive("heading", { level: 1 }) ? "is-active" : ""}
+        className={editor.isActive('heading', { level: 1 }) ? 'is-active' : ''}
       >
         h1
       </button>
       <button
         onClick={() => editor.chain().focus().toggleHeading({ level: 2 }).run()}
-        className={editor.isActive("heading", { level: 2 }) ? "is-active" : ""}
+        className={editor.isActive('heading', { level: 2 }) ? 'is-active' : ''}
       >
         h2
       </button>
       <button
         onClick={() => editor.chain().focus().toggleHeading({ level: 3 }).run()}
-        className={editor.isActive("heading", { level: 3 }) ? "is-active" : ""}
+        className={editor.isActive('heading', { level: 3 }) ? 'is-active' : ''}
       >
         h3
       </button>
       <button
         onClick={() => editor.chain().focus().toggleHeading({ level: 4 }).run()}
-        className={editor.isActive("heading", { level: 4 }) ? "is-active" : ""}
+        className={editor.isActive('heading', { level: 4 }) ? 'is-active' : ''}
       >
         h4
       </button>
       <button
         onClick={() => editor.chain().focus().toggleHeading({ level: 5 }).run()}
-        className={editor.isActive("heading", { level: 5 }) ? "is-active" : ""}
+        className={editor.isActive('heading', { level: 5 }) ? 'is-active' : ''}
       >
         h5
       </button>
       <button
         onClick={() => editor.chain().focus().toggleHeading({ level: 6 }).run()}
-        className={editor.isActive("heading", { level: 6 }) ? "is-active" : ""}
+        className={editor.isActive('heading', { level: 6 }) ? 'is-active' : ''}
       >
         h6
       </button>
       <button
         onClick={() => editor.chain().focus().toggleBulletList().run()}
-        className={editor.isActive("bulletList") ? "is-active" : ""}
+        className={editor.isActive('bulletList') ? 'is-active' : ''}
       >
         bullet list
       </button>
       <button
         onClick={() => editor.chain().focus().toggleOrderedList().run()}
-        className={editor.isActive("orderedList") ? "is-active" : ""}
+        className={editor.isActive('orderedList') ? 'is-active' : ''}
       >
         ordered list
       </button>
       <button
         onClick={() => editor.chain().focus().toggleCodeBlock().run()}
-        className={editor.isActive("codeBlock") ? "is-active" : ""}
+        className={editor.isActive('codeBlock') ? 'is-active' : ''}
       >
         code block
       </button>
       <button
         onClick={() => editor.chain().focus().toggleBlockquote().run()}
-        className={editor.isActive("blockquote") ? "is-active" : ""}
+        className={editor.isActive('blockquote') ? 'is-active' : ''}
       >
         blockquote
       </button>
@@ -118,11 +119,11 @@ const MenuBar = ({ editor }: { editor: Editor | null }) => {
 };
 
 function App() {
-  const [state, setState] = useState("<p>Hello World!</p>");
+  const [state, setState] = useState('<p>Hello World!</p>');
   const editor = useEditor({
     extensions: [
       StarterKit.configure({
-        heading: { HTMLAttributes: { class: "heading" } },
+        heading: { HTMLAttributes: { class: 'heading' } },
       }),
     ],
     content: state,
