66e76e63b1970803cdc1fcbe90401ede43c066de
devj <power3xz@gmail.com>
2022-07-16 11:23:47 +0900

    bold 버튼 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
-import { useEditor, EditorContent } from "@tiptap/react";
+import { useEditor, EditorContent, Editor } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 import { useEffect } from "react";
 import "./App.css";
+
+const MenuBar = ({ editor }: { editor: Editor | null }) => {
+  if (!editor) {
+    return null;
+  }
+  return (
+    <>
+      <button
+        onClick={() => editor.chain().focus().toggleBold().run()}
+        className={editor.isActive("bold") ? "is-active" : ""}
+      >
+        bold
+      </button>
+    </>
+  );
+};
 
 function App() {
   const editor = useEditor({
@@ -15,7 +31,12 @@ function App() {
       });
     }
   }, [editor]);
-  return <EditorContent editor={editor} />;
+  return (
+    <>
+      <MenuBar editor={editor} />
+      <EditorContent editor={editor} />
+    </>
+  );
 }
 
 export default App;
